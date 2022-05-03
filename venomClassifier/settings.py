42fdfc29e0711b050c3c092a41ec2d90673cf52f
Saul Trujillo <bryan.trujillo7470@alumnos.udg.mx>
2022-05-03 08:54:22 -0500

    static files

--- venomClassifier/settings.py
+++ venomClassifier/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pfmm@h33r2ddvrttv70cjh!g-%ml9hx1-9k@=5mgrhm1!1iys!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
