243dd14f55bc16e28e985f30a3f675f154076219
Saul Trujillo <bryan.trujillo7470@alumnos.udg.mx>
2022-05-03 05:45:28 -0500

    csrf

--- manageUsers/views.py
+++ manageUsers/views.py
@@ -1,26 +1,21 @@
 from dataclasses import dataclass
 from django.shortcuts import redirect, render
-from django.template import RequestContext
 from django.views.generic import View
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.views.decorators.csrf import csrf_protect
-from django.template.context_processors import csrf
 
 
 class viewRegister(View):
     
     def get(self, request):
         form=UserCreationForm()
-        form = RequestContext(form)
-        return render(request, "manageUsers/register.html", form)
+        return render(request, "manageUsers/register.html", {"form":form})
 
     def post(self, request):
         form=UserCreationForm(request.POST)
 
-        form = RequestContext(form)
         if form.is_valid():
             user=form.save()
             login(request, user)
@@ -31,14 +26,13 @@ class viewRegister(View):
             for msg in form.error_messages:
                 messages.error(request, form.error_messages[msg])
             
-            return render(request, "manageUsers/register.html", form)
+            return render(request, "manageUsers/register.html", {"form":form})
 
 
 def close_session(request):
     logout(request)
 
     return redirect('Home')
-
 
 def logIn(request):
     if request.method=="POST":
@@ -58,6 +52,5 @@ def logIn(request):
             messages.error(request, "Incorrect Username or Password")
 
     form=AuthenticationForm()
-    form = RequestContext(form)
-    return render(request, "manageUsers/login.html", form)
+    return render(request, "manageUsers/login.html", {"form":form})
 
